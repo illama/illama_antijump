13a9f1ebd62440da94b810fde9e6859c8e281939
illama <10000maxence@gmail.com>
2024-12-20 21:27:05 -0500

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game 'gta5'
 
 name 'illama_antijump'
-description 'Système simple pour empêcher le spam de sauts pour la version 1.1X.X de ESX.'
+description 'Un script visant à limiter les sauts pour rendre les mouvements des joueurs plus réalistes sur les serveurs FiveM. Compatible avec ESX et facilement configurable.'
 author 'Illama'
 version '1.0.0'
 
